fae5f7d8b7aeb1fdc0c42c4dea00aad87e8513a7
mjturt <maks.turtiainen@gmail.com>
2020-11-18 01:45:29 +0200

    Modify temp script

--- statusscripts/polybar/temp.sh
+++ statusscripts/polybar/temp.sh
@@ -4,7 +4,7 @@
 if [ "$1" == "--ryzen" ]; then
     TEMP=$(sensors | grep "Tctl" | awk '{print $2}' | tr -d "+°C")
 else
-    TEMP=$(sensors | grep "^temp" | awk '{print $2}' | tr -d "+°C")
+    TEMP=$(sensors | grep "^Package id 0" | awk '{print $4}' | tr -d "+°C" | head -1)
 fi
 
 if (($(echo "$TEMP < 60" | bc -l))); then
@@ -12,7 +12,7 @@ if (($(echo "$TEMP < 60" | bc -l))); then
 elif (($(echo "$TEMP < 75" | bc -l))); then
     SYMBOL="%{F#f57800}%{F-}"
 else
-    SYMBOL="%{F#d300c4}%{F-}"
+    SYMBOL="%{F#ff0000}%{F-}"
 fi
 
 echo -e "$SYMBOL $TEMP°C"
